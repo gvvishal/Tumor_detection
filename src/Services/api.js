74902afa603718e0527a7cd5f4b74d24b7a1aec2
Vishal GV <125007697+gvvishal@users.noreply.github.com>
2025-08-29 12:53:32 +0530

    api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,13 +1,6 @@
-export const getPrediction = async (formData) => {
-  try {
-    const response = await fetch("http://localhost:5000/predict", {
-      method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    return data.result; // expects { result: "No Tumor" } from backend
-  } catch (error) {
-    console.error("Error:", error);
-    return "Error in prediction";
-  }
-};
+const API_URL = "https://vip0jliz09.execute-api.ap-south-1.amazonaws.com/default/MyfirstLambda";
+
+export async function testLambda() {
+  const res = await fetch(API_URL);
+  return res.json();
+}
